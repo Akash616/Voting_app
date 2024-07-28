f2fade6a700a43bc841aa9418d70dc0ddbe6e309
akash <61616akash@gmail.com>
2024-07-28 11:02:51 +0530

    candidate routes

--- server.js
+++ server.js
@@ -9,8 +9,11 @@ const PORT = process.env.PORT || 3000;
 
 //Import the router files
 const userRoutes = require('./routes/userRoutes');
+const candidateRoutes = require('./routes/candidateRoutes');
+
 //Use the routers
 app.use('/user', userRoutes);
+app.use('/candidate', candidateRoutes);
 
 app.listen(PORT, () => {
     console.log('listening on port 3000');
